7fff3a5b3e05d0d4fa24c625d1b582420d65a3e1
Michał Stanek <michal.stanek@me.com>
2017-05-21 16:30:51 +0200

    FOS

---
+++ car_status/src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin/addTable")
+     */
+    public function addTableAction()
+    {
+        return $this->render('AppBundle:Admin:add_table.html.twig', array(
+            // ...
+        ));
+    }
+
+    /**
+     * @Route("/admin/deleteTable")
+     */
+    public function deleteTableAction()
+    {
+        return $this->render('AppBundle:Admin:delete_table.html.twig', array(
+            // ...
+        ));
+    }
+
+}
---
+++ car_status/src/AppBundle/Tests/Controller/AdminControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AdminControllerTest extends WebTestCase
+{
+    public function testAddtable()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', 'admin/addTable');
+    }
+
+    public function testDeletetable()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', 'admin/deleteTable');
+    }
+
+}
