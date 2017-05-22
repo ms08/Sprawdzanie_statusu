4e4d25c69775bf4014713d51cdadbfcbf0283f0a
Michał Stanek <michal.stanek@me.com>
2017-05-22 19:52:07 +0200

    Added CSV file

--- car_status/src/AppBundle/Controller/AdminController.php
+++ car_status/src/AppBundle/Controller/AdminController.php
@@ -4,16 +4,34 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\Form\FormBuilder;
 
 class AdminController extends Controller
 {
     /**
      * @Route("/admin/addTable")
      */
-    public function addTableAction()
+    public function addTableAction(Request $request)
     {
+
+        $data = [];
+        $form = $this -> createFormBuilder($data)
+            ->add('file',FileType::class,array('label'=>'Plik'))
+        ->getForm();
+        
+
+
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            return new Response('ok');
+        }
+
         return $this->render('AppBundle:Admin:add_table.html.twig', array(
-            // ...
+           'form'=>$form->createView()
         ));
     }
 
